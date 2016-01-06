4de59ff689779dc2346b655077b6332c090cc2ce
Yoeri Van Damme <Yoeri Van Damme>
2016-01-06 12:00:25 +0100

    added protractor

--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -1,21 +1,22 @@
 'use strict';
 
 describe('The main view', function () {
-  var page;
 
-  beforeEach(function () {
-    browser.get('/index.html');
-    page = require('./main.po');
-  });
+    beforeEach(function () {
+        browser.get('/index.html');
+    });
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
-  });
+    it('should disable the submit button when the form is invalid', function () {
+        var addButton = element(by.css('button'));
+        expect(addButton.isEnabled()).toBe(false);
+    });
+    it('should enable the submit button when the form is valid', function () {
+        element(by.model('main.newContact.firstName')).sendKeys('Frits');
+        element(by.model('main.newContact.surname')).sendKeys('Spits'); element(by.model('main.newContact.email')).sendKeys('frits@spits.nl');
 
-  it('should list more than 5 awesome things', function () {
-    expect(page.thumbnailEls.count()).toBeGreaterThan(5);
-  });
+        var addButton = element(by.css('button'));
+        expect(addButton.isEnabled()).toBe(true);
+    });
+
 
 });
